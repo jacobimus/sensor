da724cb8b00651bc55936e8cff6b64d230b8ab6f
jacobimus <jacobo.uribe.bedoya@gmail.com>
2025-02-03 20:35:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showNumber(input.lightLevel())
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
